1b5c29eff62f7c3f4f57214fcca762fc454a8dbc
DeepanshKhurana <deepanshkhurana@outlook.com>
2019-03-11 08:00:55 +0000

    csvToDataframe - POC for other approach

--- R/csvToDataframe.R
+++ R/csvToDataframe.R
@@ -5,5 +5,10 @@
 #' @examples csvToDataframe("s3://cloudsimplifier-examples/example.csv")
 
 csvToDataframe <- function(s3URL) {
-  return(readr::read_csv(aws.s3::get_object(s3URL, url_style = "virtual", check_region = FALSE)))
+  # return(readr::read_csv(
+  #   aws.s3::get_object(s3URL, url_style = "virtual", check_region = FALSE)
+  # ))
+  return(read.csv(
+    aws.s3::get_object(s3URL, url_style = "virtual", check_region = FALSE)
+  ))
 }
